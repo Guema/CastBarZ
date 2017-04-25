5b067c739d8756c417be596b514efcb607901b16
Guema <floorent1993@gmail.com>
2017-04-25 06:27:45 +0200

    Working prototype of castbar

--- gCastBars.lua
+++ gCastBars.lua
@@ -23,30 +23,33 @@ function Addon:OnInitialize()
     --_G.CastingBarFrame.ignoreFramePositionManager = true
     _G.CastingBarFrame:UnregisterAllEvents()
     LSM:Register("statusbar", "Solid", "Interface\\AddOns\\"..AddonName.."\\Media\\Solid")
-
-    self.castbars = {}
-    function self.castbarsAddCastBar(frame)
-
-    end
 end
 
 function Addon:OnEnable()
     self.CastingBarFrame = self:CreateClass("StatusBar")
     local f = self.CastingBarFrame
+    f:Hide()
+    f:SetStatusBarTexture("Interface\\AddOns\\"..AddonName.."\\Media\\Solid")
     f:SetSize(220, 24)
     f:SetPoint("BOTTOM", 0, 170)
-
-    f:RegisterUnitEvent("UNIT_SPELLCAST_START", "player", function(_, event, _, name, _)
-        print("Casting ".. name .. "...")
+    f:SetFillStyle("STANDARD")
+    f:SetMinMaxValues(0.0, 1.0)
+    f.t = f:CreateTexture(nil)
+    f.t:SetColorTexture(0, 0, 0)
+    f.t:SetAllPoints(f)
+    
+    f:RegisterUnitEvent("UNIT_SPELLCAST_START", "player", function(self, event, unit, name, ...)
+        self:Show()
     end)
 
-    f:RegisterUnitEvent("UNIT_SPELLCAST_SUCCEEDED", "player", function()
-        print("Succes !")
+    f:RegisterUnitEvent("UNIT_SPELLCAST_STOP", "player", function(self, event, unit, name, ...)
+        self:Hide()
     end)
 
-    f:RegisterUnitEvent("UNIT_SPELLCAST_STOP", "player", function()
-        print("Stopped.")
+    f:SetScript('OnUpdate', function(self, rate)
+        local _, _, _, _, startTime, endTime = UnitCastingInfo("player")
+        if startTime and endTime then
+            f:SetValue((GetTime()*1000 - startTime) / (endTime-startTime))
+        end
     end)
-
-    f:UnregisterEvent("UNIT_SPELLCAST_STOP")
 end
