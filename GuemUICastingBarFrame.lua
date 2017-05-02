73d7273bcb8663a3dfa8d270db2e8154b336fa42
Guema <floorent1993@gmail.com>
2017-05-02 17:34:19 +0200

    Created a first experiment of what 3d animation could be.
    - To do : Hide when Stop casting
    - To do : Test for performances
    - To do : See what Slider or MaskTexture could do.

--- GuemUICastingBarFrame.lua
+++ GuemUICastingBarFrame.lua
@@ -54,6 +54,29 @@ function Addon:CreateClass(Class, Name, Parent)
     return obj
 end
 
+function Addon:CreatePlayerModel(Name, Parent, Region)
+    Name = Name or nil
+    Parent = Parent or UIParent
+
+    local m = CreateFrame("PlayerModel", Name, Parent)
+    m:SetPoint("CENTER", Parent, "CENTER")
+    m:SetSize(Parent:GetSize())
+    m:SetModel("Spells\\Lightning_Area_Disc_State.M2")
+    m:SetModelScale(Parent:GetEffectiveScale())
+    m:SetSequence(28)
+    m:SetPosition(0.0, 0.0, 0.0)
+    m:SetFrameLevel(3)
+    m:Show()
+
+    m:SetScript("OnShow", function(self)
+        self:SetModel("Spells\\Lightning_Area_Disc_State.M2")
+    end)
+
+    return m
+end
+
+
+
 function Addon:CreateCastingBarFrame(Unit, Parent)
     assert(type(Unit) == "string", "Usage : CreateCastingBarFrame(string Unit)")
     Parent = Parent or UIParent
@@ -62,15 +85,16 @@ function Addon:CreateCastingBarFrame(Unit, Parent)
     --local spark = s:CreateTexture(spark)
     
     f:Hide()
-    f:SetSize(220, 24)
+    f:SetSize(230, 25)
     f:SetPoint("BOTTOM", 0, 170)
     local t = f:CreateTexture()
     t:SetColorTexture(0, 0, 0)
     t:SetAllPoints(f)
-    
-    s:SetAllPoints(f)
+
+    s:SetPoint("LEFT", f, "LEFT")
+    s:SetSize(f:GetSize())
     s:SetStatusBarTexture("Interface\\AddOns\\"..AddonName.."\\Media\\Solid")
-    s:SetStatusBarColor(0, 0.7, 1.0)
+    s:SetStatusBarColor(0, 0.4, 7.0)
     s:SetFillStyle("STANDARD")
     s:SetMinMaxValues(0.0, 1.0)
 
@@ -87,7 +111,22 @@ function Addon:CreateCastingBarFrame(Unit, Parent)
     alpha:SetDuration(0.5)
     alpha:SetFromAlpha(1.0)
     alpha:SetToAlpha(0.0)
-    
+
+    local m = self:CreatePlayerModel(nil, f)
+
+    local scrollframe = CreateFrame("ScrollFrame", nil, f)
+    scrollframe:SetScrollChild(m)
+    scrollframe:SetPoint("LEFT", f, "LEFT")
+    scrollframe:SetHeight(f:GetHeight())
+    scrollframe:SetWidth(0)
+    scrollframe:SetVerticalScroll(0)
+    scrollframe:SetHorizontalScroll(0)
+
+    f.fadein:SetScript("OnPlay", function(self, ...)
+        f.fadeout:Stop()
+        f:Show()
+    end)
+
     f.fadeout:SetScript("OnFinished", function(self, ...)
         f:Hide()
     end)
@@ -96,8 +135,6 @@ function Addon:CreateCastingBarFrame(Unit, Parent)
 
     f:RegisterUnitEvent("UNIT_SPELLCAST_START", Unit, function(self, event, unit, ...)
         c.name, _, c.text, c.texture, c.startTime, c.endTime, _, c.castID, c.notInterruptible = UnitCastingInfo(unit)
-        self:Show()
-        self.fadeout:Stop()
         self.fadein:Play()
     end)
 
@@ -109,18 +146,22 @@ function Addon:CreateCastingBarFrame(Unit, Parent)
         local val = s:GetMinMaxValues()
         c = {}
         s:SetValue(val)
+        scrollframe:SetWidth(0)
+        scrollframe:UpdateScrollChildRect()
     end)
 
     f:RegisterUnitEvent("UNIT_SPELLCAST_SUCCEEDED", Unit, function(self, event, unit, name, rank, castid, spellid)
         if(castid == c.castid) then
             local _, val = s:GetMinMaxValues()
             s:SetValue(val)
+            scrollframe:SetWidth(15)
         end
     end)
 
     f:SetScript('OnUpdate', function(self, rate)
         if c.startTime and c.endTime then
             s:SetValue((GetTime()*1000 - c.startTime) / (c.endTime-c.startTime))
+            scrollframe:SetWidth(s:GetStatusBarTexture():GetWidth())
         end
     end)
 
--- gCastBars.lua
+++ gCastBars.lua
@@ -27,4 +27,52 @@ end
 
 function Addon:OnEnable()
     self.CastingBar = self:CreateCastingBarFrame("player")
+
+    --self.Model:Hide()
+    --[[
+    -- Create a container frame
+    local f = AceGUI:Create("Frame")
+    f:SetCallback("OnClose",function(widget) AceGUI:Release(widget) end)
+    f:SetTitle("AceGUI-3.0 Example")
+    f:SetStatusText("Status Bar")
+    f:SetLayout("Flow")
+    --local btn = AceGUI:Create("Button")
+    --btn:SetWidth(170)
+    --btn:SetText("Button !")
+    --btn:SetCallback("OnClick", function() print("Click!") end)
+    -- Add the button to the container
+    --f:AddChild(btn)
+    local sliderX, sliderY, sliderZ = AceGUI:Create("Slider"), AceGUI:Create("Slider"), AceGUI:Create("Slider")
+
+    sliderX:SetSliderValues(-50, 50, 0.01)
+    sliderY:SetSliderValues(-50, 50, 0.01)
+    sliderZ:SetSliderValues(-50, 50, 0.01)
+
+    sliderX:SetValue(0)
+    sliderY:SetValue(0)
+    sliderZ:SetValue(0)
+
+    f:DoLayout()
+    
+    local function displayResult()
+        print(m:GetPosition())
+    end
+
+    local function callback()
+        m:SetPosition(sliderX:GetValue(), sliderY:GetValue(), sliderZ:GetValue())
+        displayResult()
+    end
+
+    sliderX:SetCallback("OnValueChanged", callback)
+    sliderY:SetCallback("OnValueChanged", callback)
+    sliderZ:SetCallback("OnValueChanged", callback)
+
+    sliderX:SetCallback("OnMouseUp", displayResult)
+    sliderY:SetCallback("OnMouseUp", displayResult)
+    sliderZ:SetCallback("OnMouseUp", displayResult)
+    
+    f:AddChild(sliderX)
+    f:AddChild(sliderY)
+    f:AddChild(sliderZ)
+    --]]
 end
