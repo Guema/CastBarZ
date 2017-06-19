1a3222956ac90a872760ea6b600276d91489c3cb
Guema <floorent1993@gmail.com>
2017-06-19 14:36:24 +0200

    fixed some build bugs, and enhanced to be compatibility with pkgmeta.yaml

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 // Constants
 
-const NODEJS_EXCLUDE_PATTERN = ["!build{,/**}", "!node_modules{,/**}"] // ignore folder and all its hierarchy
-const CONFIG_FILES = ["pkgmeta.yaml", "debugconfig.yaml"]
+const NODEJS_EXCLUDE_PATTERN = ["!build{,/**}", "!node_modules{,/**}", "!pkgmeta.yaml"] // ignore folder and all its hierarchy
+const CONFIG_FILE = "pkgmeta.yaml"
 
 // Requiring node packages :
 
@@ -13,45 +13,37 @@ var fs = require("fs");
 
 // Variables :
 
-var config = {};
-
-function loadconfig(){
-    config = {}
-    CONFIG_FILES.map(function(element){
-        var file = yaml.safeLoad(fs.readFileSync(element));
-        for(var k in file){
-            if(!config[k]){
-                config[k] = file[k]
-            }
-            else if(config[k] instanceof Array){
-                config[k] = config[k].concat(file[k])
-            }
-        }
-    }); 
+var config;
+function reload()
+{
+    config = yaml.safeLoad(fs.readFileSync(CONFIG_FILE));
 }
+reload();
 
 //Load config file task
-gulp.task('build', ['clean'], function() {
-    var paths = ["**"]
-    .concat(config.ignore.map(function(element) { return "!" + element}))
+gulp.task('build', function() {
+    var paths = ["**"].concat((config.ignore.concat(config.test_build.ignore)).map(function(element) { return "!" + element}))
     .concat(NODEJS_EXCLUDE_PATTERN);
-    return gulp.src(paths, {base: '.'}).pipe(combine(config.debug_deploy_paths.map(function(element) {
+    return gulp.src(paths, {base: '.'}).pipe(combine(config.test_build.paths.map(function(element) {
         return gulp.dest(element + '/' + config["package-as"]);
     })));
 });
 
 gulp.task('clean', function() {
-    return del(config.debug_deploy_paths.map(function(element) {
-        return element + '/' + config["package-as"] 
-    }));
-})
-
-gulp.task('watch', function() {
-    var watcher = gulp.watch("**", {interval : 1000}, ['build']);
-    watcher.on('change', function(event) {
-        console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
-    });
-    return watcher;
+    return del(config.test_build.paths.map(function(element) {
+        return element + '/' + config["package-as"] + '/*';
+    }), {force: true});
 });
 
-gulp.task('default', ['build', 'watch']);
+gulp.task('watch', function() {
+    var w1 = gulp.watch("pkgmeta.yaml", { base: ".", interval : 200 }, function(){ gulp.start("clean"); reload(); gulp.start("build"); });
+    var w2 = gulp.watch(["**"].concat(NODEJS_EXCLUDE_PATTERN).concat("!pkgmeta.yaml") , { base: ".", interval : 200 }, ["clean", "build"]);
+    w1.on('change', function(event) {
+        console.log('Config File ' + event.path + 'was ' + event.type + ', reloading config...' );
+    });
+    w2.on('change', function(event) {
+        console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+    });
+});
+
+gulp.task('default', ['watch']);
